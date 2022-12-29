ec47fe394b853f65beebcc0166042fdff968d5c3
Vishva1407 <mvishva1407@gmail.com>
2022-12-30 00:27:49 +0530

    linear search modified

--- sorting/linear_search.py
+++ sorting/linear_search.py
@@ -1,7 +1,14 @@
 def linear_search(key,num_list):
-    for i in num_list:
-        if i == key:
-            return 1
+    for i,num in enumerate(num_list):
+        if num == key:
+            return 1,i
     return 0
 
-print(linear_search(1,[5,4,3,2,1]))
+if __name__ == "__main__":
+    num_list = [1,2,3,4,5,6,7]
+    key = 5
+    found,index = linear_search(key,num_list)
+    if found:
+        print("The given element is found at ",index)
+    else:
+        print("The element is not found")
